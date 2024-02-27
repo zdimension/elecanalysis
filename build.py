473f8b06a12a261806c0c36dc403bb727ee60f9a
Tom Niget <zippedfire@free.fr>
2024-02-27 13:36:15 +0100

    date fmt

--- build.py
+++ build.py
@@ -4,10 +4,14 @@ from pathlib import Path
 import nicegui
 import sys
 
-# get current commit hash and write to _version.py
-commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()
+# get current commit hash and date
+commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip().decode('utf-8')
+commit_hash = commit_hash[:8]
+commit_date = subprocess.check_output(['git', 'show', '-s', '--format=%ci', 'HEAD']).strip().decode('utf-8')
+commit_date = commit_date.split(' ')[0]
+version = f'{commit_hash} ({commit_date})'
 with open('_version.py', 'w') as f:
-    f.write(f'__version__ = "{commit_hash}"\n')
+    f.write(f'__version__ = "{version}"\n')
 
 cmd = [
     sys.executable,
